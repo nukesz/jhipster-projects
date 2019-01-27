a17a5a3955ead7355b70555420bc037fd95d4bdc
nukesz <norbert.benczur@gmail.com>
2019-01-27 18:30:23 +0100

    Generated online store entity model

---
+++ e-commerce-app/online-store/src/main/java/com/nukesz/store/repository/ShipmentRepository.java
@@ -0,0 +1,15 @@
+package com.nukesz.store.repository;
+
+import com.nukesz.store.domain.Shipment;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Shipment entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+}
